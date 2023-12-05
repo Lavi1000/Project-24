3c4d97e85a4ea7e607b0573c1bd18c62572858e6
Lavi1000 <145509294+Lavi1000@users.noreply.github.com>
2023-12-05 20:50:19 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -93,28 +93,28 @@ function draw() {
 //   }
 // }
 
-function keyPressed() {
-  if (keyCode === 23) {
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle;
-    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+//function keyPressed() {
+//  if (keyCode === 23) {
+ //   var posX = playerArcher.body.position.x;
+ //   var posY = playerArcher.body.position.y;
+ //   var angle = playerArcher.body.angle;
+  //  var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
 
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);  }
-  }
+ //   Matter.Body.setAngle(arrow.body, angle);
+//    playerArrows.push(arrow);  }
+ // }
 
-  // function keyPressed() {
-  //   if (keyCode === 32) {
-  //     var posX = playerArcher.body.position.x;
-  //     var posY = playerArcher.body.position.y;
-  //     var angle = playerArcher.body.angle;
-  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+   function keyPressed() {
+     if (keyCode === 32) {
+       var posX = playerArcher.body.position.x;
+       var posY = playerArcher.body.position.y;
+     var angle = playerArcher.body.angle;
+       var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
 
-  //     Matter.Body.setAngle(arrow.body, angle);
-  //     playerArrows.push(arrow);
-  //   }
-  // }
+       Matter.Body.setAngle(arrow.body, angle);
+       playerArrows.push(arrow);
+     }
+ }
 
   // function keyPressed() {
   //   if (keyCode === 32) {
